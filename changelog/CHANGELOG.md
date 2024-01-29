03b7e9f11fccc5b7d776dd587c0e572568b0f4ec
kk.z <kk.z@qycocu.com>
2024-01-29 17:37:50 +0800

    add self_service-worker and use service-worker fetch-event to reload cache resource

---
+++ changelog/CHANGELOG.md
@@ -0,0 +1,11 @@
+### start websocket
+
+```bash
+1. python3 websocket_server.py
+2. npm run dev
+```
+
+### use service-worker
+
+使用 service-worker fetch，获取的 urlsToCache 的缓存资源
+![sw_cache](./imgs/sw_cache.png)
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,34 @@
-// eslint-disable-next-line no-unused-vars
+/**
+ * Service-worker 主要用于网路请求的拦截和缓存策略，
+ * 不能直接创建或管理 websocket连接
+ */
+const CACHE_NAME = 'vue-app-cache'
+const urlsToCache = [
+  '/',
+  '/index.html'
+  //  其他需要缓存的资源
+]
+
 self.addEventListener('install', event => {
   console.log('Service Worker installing.')
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Opened cache: ', cache)
+      return cache.addAll(urlsToCache)
+    })
+  )
 })
 
 // eslint-disable-next-line no-unused-vars
 self.addEventListener('activate', event => {
   console.log('Service Worker activated')
+})
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) return response
+      return fetch(event.request)
+    })
+  )
 })
